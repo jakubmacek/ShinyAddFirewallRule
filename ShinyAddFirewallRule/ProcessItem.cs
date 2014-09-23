afba4bf4c3278bce6d9e0cb07cfaea96f4802e80
Jakub Macek <1@jakubmacek.cz>
2014-09-24 00:01:51 +0200

    Initial commit from unversioned files, added basic documentation.

---
+++ ShinyAddFirewallRule/ProcessItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ShinyAddFirewallRule
+{
+    class ProcessItem
+    {
+        public Process Process { get; private set; }
+
+        public ProcessItem(Process process)
+        {
+            Process = process;
+        }
+
+        public override string ToString()
+        {
+            try
+            {
+                return Process.MainModule.FileName;
+            }
+            catch
+            {
+                return "-- access denied -- ";
+            }
+        }
+    }
+}
